52e6cc3dfff779da1c9aeb00ed7ee67aa265a62f
monikamurugan <84066785+monikamurugan@users.noreply.github.com>
2021-08-28 14:19:05 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int add(int);
+int add_rec(int);
+
+int main()
+{
+    int num;
+
+    printf("Enter number\n");
+    scanf("%d", &num);
+
+    printf("Sum of Digits(without using recursion): %d\n", add(num));
+    printf("Sum of Digits(using recursion): %d\n", add_rec(num));
+
+    return 0;
+}
+
+int add(int num)
+{
+    int sum = 0;
+
+    while(num)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+
+    return(sum);
+}
+
+int add_rec(int num)
+{
+    if(num)
+        return( (num % 10) + add_rec(num / 10) );
+    else
+        return 0;
+}
